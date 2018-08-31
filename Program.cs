45a0dec37772582bbf47b3ceeb81823266f57237
vinda <vinda@afrizal.com>
2018-08-30 20:55:11 -0700

    projeck vindaku

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int pilihan;
+            do
+            {
+                Console.WriteLine();
+                Console.Clear();
+                Console.WriteLine("WEARNESS EDUCATION CENTER MADIUN");
+                Console.WriteLine("Informatika 1 - 2018");
+                Console.WriteLine("--------------------------------");
+
+                do
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("Pengolahan Data Siswa");
+                    Console.Clear();
+                    Console.WriteLine(":::Pengolahan Data Siswa:::");
+                    Console.WriteLine("---------------------------");
+                    Console.WriteLine("1.Tambah Data Siswa");
+                    Console.WriteLine("2.Tampil Data Siswa");
+                    Console.WriteLine("3.Edit Data Siswa");
+                }
+            }
+        }
+    }
+}
